bcfe169a9792ec406ab6399d8637ccf67da26982
Oluwagbolawole Bamidele <oluwagbolawole.bamidele@pau.edu.ng>
2024-11-03 17:31:46 +0100

    week three's commit

---
+++ week-3/practice_2/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let rusult = 10;
+    let age: u32 = 20;
+    let sum: i32 = 5 - 15; 
+
+    println!("Result value is {}", rusult);
+    println!("Age is {}", age);
+    println!("sum is {}", sum);
+    
+}
