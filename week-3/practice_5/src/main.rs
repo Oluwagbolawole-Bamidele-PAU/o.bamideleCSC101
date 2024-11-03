f15c508ca5e6e8b42ebb30ffed4cbece0d28b811
Oluwagbolawole Bamidele <oluwagbolawole.bamidele@pau.edu.ng>
2024-11-03 19:46:23 +0100

    (week threes'a commit)

--- week-3/practice_5/src/main.rs
+++ week-3/practice_5/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let special_character = '0';
+    let alphabet: char = 'g';
+    let surname ="Bolaji";
+    let first_name = "michael";
+    let middle_name = "Gooyega";
+
+    println!("");
+    println!("Special Character: {}", special_character);
+    println!("alphabet; {}", alphabet);
+    println!("Name: {} {} {}", surname, middle_name, first_name );
 }
